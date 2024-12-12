72cc6db528f1fe8c75a33ea824443b4c09dc844c
heynar-max <heynar76@hotmail.com>
2024-12-11 20:33:08 -0500

    Pantalla de producto

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,58 @@
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
 
-export default function productSlugShopPage() {
+
+    interface Props {
+        params: {
+            slug: string;
+        }
+    }
+
+
+export default function productSlugShopPage({params}: Props) {
+
+    const { slug } = params;
+    const product = initialData.products.find( product => product.slug === slug);
+
+    if ( !product ) {
+        notFound();
+    }
+
     return (
-        <div>
-        <h1>product pagr</h1>
+        <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+
+            {/* slideshow  */}
+            <div className="col-span-1 md:col-span-2" >
+                hola
+            </div>
+
+            {/* detalles  */}
+            <div className="col-span-1 px-5">
+                <h1 className="antialiased font-bold text-xl">
+                    {product.title}
+                </h1>
+                <p className="text-lg mb-5">
+                    $ {product.price}
+                </p>
+
+                {/* selector de tallas  */}
+
+                {/* selector de cantidad  */}
+
+                {/* boton  */}
+                <button className="btn-primary my-5">
+                    agregar al carrito
+                </button>
+
+                {/* descripcion  */}
+                <h3 className="font-bold text-sm">
+                    descripción
+                </h3>
+                <p className="font-light">
+                    {product.description}
+                </p>
+            </div>
+
         </div>
     );
 }
