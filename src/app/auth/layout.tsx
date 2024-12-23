606371fd488d75083987bb4358515f8fbc481dc3
heynar-max <heynar76@hotmail.com>
2024-12-23 18:43:33 -0500

    Login - Registro y Auth Layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,8 +3,12 @@ export default function AuthLayout({children}: {
     children: React.ReactNode;
 }) {
     return (
-        <main className="min-h-screen bg-slate-500">
-            {children}
+        <main className="flex justify-center">
+            <div className="w-full sm:w-[350px] px-10">
+
+                { children }
+
+            </div>
         </main>
     );
 }
