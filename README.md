67e6c1ca14ca0c0334f1a582fe3c0b6d867454e0
heynar-max <heynar76@hotmail.com>
2025-03-13 20:26:07 -0500

    Prisma Client - Borrar contenido de tablas

--- README.md
+++ README.md
@@ -6,7 +6,8 @@
 1. Clonar el repositorio
 2. Instalar dependencias ```npm install```
 3. Correr las migraciones de Prisma ```npx prisma migrate dev```
-4. Correr el proyecto 
+4. ejecutar seed ```npm run seed```
+5. Correr el proyecto 
 ```bash
 npm run dev
 # or
---
+++ src/seed/seed-database.ts
@@ -0,0 +1,20 @@
+import { initialData } from "./seed";
+import { prisma } from '../lib/prisma';
+
+
+async function main() {
+
+    // 1. Borrar registros previos
+        await Promise.all( [
+        prisma.productImage.deleteMany(),
+        prisma.product.deleteMany(),
+        prisma.category.deleteMany(),
+    ]);
+
+    console.log('Seed Ejecutado');
+}
+
+
+(() => {
+    main();
+})();
