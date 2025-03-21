fa5ebbf86fdc180880008f80aebec720390edd2b
heynar-max <heynar76@hotmail.com>
2025-03-21 17:30:37 -0500

    Controlar cuando no hay productos en la página

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,5 +1,6 @@
 import { ProductGrid, Title } from "@/components";
 import { getPaginatedProductsWithImages } from "./actions";
+import { redirect } from "next/navigation";
 
 interface Props {
   searchParams:{
@@ -11,6 +12,10 @@ export default async function Home( { searchParams}:  Props) {
 
   const page = searchParams.page ? parseInt (searchParams.page) : 1;
   const{ products } = await getPaginatedProductsWithImages({ page });
+
+  if ( products.length === 0 ) {
+    redirect('/');
+  }
 
 
   return (
