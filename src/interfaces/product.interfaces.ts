0d59b03e5323c502c3b67a2380cfefdf440947a6
heynar-max <heynar76@hotmail.com>
2025-04-03 22:00:01 -0500

    Client Side - Cart Store

--- src/interfaces/product.interfaces.ts
+++ src/interfaces/product.interfaces.ts
@@ -12,6 +12,16 @@ export interface Product {
     gender: Category;
 }
 
+export interface CartProduct {
+    id: string;
+    slug: string;
+    title: string;
+    price: number;
+    quantity: number;
+    size: Size;
+    image: string;
+}
+
 export type Category = 'men'|'women'|'kid'|'unisex'|'dorado'|'plateado';
 export type Size = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL'|'MUG';
 export type ValidType = 'shirts'|'pants'|'hoodies'|'hats'|'tazas';
---
+++ src/store/cart/cart-store.ts
@@ -0,0 +1,18 @@
+
+import { CartProduct } from "@/interfaces";
+import { create } from "zustand";
+
+
+interface State {
+    cart: CartProduct[];
+
+    }
+
+export const useCartStore = create<State>()(
+    
+        (set) => ({
+        cart: [],
+
+        
+    })
+);
