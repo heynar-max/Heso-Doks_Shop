3ff6db00a06a114dddbc5cc0a433ed685dbf4833
heynar-max <heynar76@hotmail.com>
2025-05-01 17:26:57 -0500

    Validar usuario - NextAuth

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,6 +2,8 @@ import type { NextAuthConfig } from 'next-auth';
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
+import { prisma } from './lib/prisma';
+import bcryptjs from 'bcryptjs';
 
 export const authConfig:NextAuthConfig = {
     pages: {
@@ -19,15 +21,18 @@ export const authConfig:NextAuthConfig = {
 
                     const { email, password } = parsedCredentials.data;
 
-                    console.log({ email, password });
-
                     // Buscar correo
-                    
+                    const user = await prisma.user.findUnique({ where: { email: email.toLowerCase() } });
+                    if ( !user ) return null;
+
                     // Comparar las contraseñas 
+                    if( !bcryptjs.compareSync( password, user.password ) ) return null;
 
-                    // Regresar el usuario 
+                    // Regresar el usuario sin el password
+                    const { password: _, ...rest } = user;
 
-                    return null;
+                    console.log({rest})
+                    return rest;
                 },
             }),
     ]
