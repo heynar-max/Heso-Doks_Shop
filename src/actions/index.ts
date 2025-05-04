0995872adcd863bff0f2b5745463bc0e182c6548
heynar-max <heynar76@hotmail.com>
2025-05-04 16:29:56 -0500

    Cerrar sesión

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,6 @@
 
 export * from './auth/login';
+export * from './auth/logout';
 
 export { getPaginatedProductsWithImages } from './product/product-pagination'
 export { getStockBySlug } from './product/get-stock-by-slug'
--- src/components/ui/siderbar/Sidebar.tsx
+++ src/components/ui/siderbar/Sidebar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { logout } from "@/actions";
 import { useUIStore } from "@/store";
 import clsx from "clsx";
 import Link from "next/link"
@@ -82,13 +83,16 @@ export const Sidebar = () => {
                     <IoLogInOutline size={ 20 } />
                     <span className="ml-3 text-xl">Ingresar</span>
                     </Link>
-                    <Link
-                    href="/"
-                    className="flex items-center mt-5 p-1 hover:bg-gray-100 rounded transition-all"
+                    <button
+                    className="flex w-full items-center mt-5 p-1 hover:bg-gray-100 rounded transition-all"
+                    onClick={() => {
+                        logout();
+                        closeMenu();
+                    }}
                     >
                     <IoLogOutOutline size={ 20 } />
                     <span className="ml-3 text-xl">Salir</span>
-                    </Link>
+                    </button>
                     {/* Line Separator */ }
                     <div className="w-full h-px bg-gray-200 my-10" />
                     <Link
