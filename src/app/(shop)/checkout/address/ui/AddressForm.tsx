937d64076e719c739a61e48abb17d2d687f0afd0
heynar-max <heynar76@hotmail.com>
2025-05-29 12:04:27 -0500

    Guardar y cargar de Zustand la dirección

--- src/app/(shop)/checkout/address/ui/AddressForm.tsx
+++ src/app/(shop)/checkout/address/ui/AddressForm.tsx
@@ -4,6 +4,8 @@
 import clsx from "clsx";
 import { useForm } from "react-hook-form"
 import { Country } from "@/interfaces";
+import { useAddressStore } from "@/store";
+import { useEffect } from "react";
 
 type FormInputs = {
     firstName: string;
@@ -24,14 +26,26 @@ interface Props {
 
 export const AddressForm = ({ countries }: Props) => {
 
-    const { handleSubmit, register, formState: { isValid } } = useForm<FormInputs>({
+    const { handleSubmit, register, formState: { isValid }, reset } = useForm<FormInputs>({
         defaultValues: {
             // Todo : leer de la base de datos 
         }
     });
 
+    const setAddress = useAddressStore( state => state.setAddress );
+    const address = useAddressStore( state => state.address );
+
+    useEffect(() => {
+        if ( address.firstName){
+            reset(address)
+        }
+    }, [])
+    
+
     const onSubmit = ( data: FormInputs ) => {
         console.log({data});
+
+        setAddress(data);
     }
 
     return (
