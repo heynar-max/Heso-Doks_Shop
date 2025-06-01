8ca4292464d39ec5f43d4cf1b21c80deb94a6da6
heynar-max <heynar76@hotmail.com>
2025-06-01 12:55:45 -0500

    Colocar orden - Total, SubTotal e Impuesto

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -35,6 +35,26 @@ export const placeOrder = async (productIds: ProductToOrder[],address: Address) 
 
     // Calcular los montos // Encabezado
     const itemsInOrder = productIds.reduce((count, p) => count + p.quantity, 0);
-    console.log(itemsInOrder)
+    
+    // Los totales de tax, subtotal, y total
+    const { subTotal, tax, total } = productIds.reduce( (totals, item) => {
+        
+        const productQuantity = item.quantity;
+        const product = products.find((product) => product.id === item.productId);
+        
+        if (!product) throw new Error(`${item.productId} no existe - 500`);
+        
+        const subTotal = product.price * productQuantity;
+        
+        totals.subTotal += subTotal;
+        totals.tax += subTotal * 0.15;
+        totals.total += subTotal * 1.15;
+        
+        return totals;
+    },
+    { subTotal: 0, tax: 0, total: 0 }
+);
+console.log(subTotal, tax, total)
+
 
 };
